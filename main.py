b802dbb4307ad57019043205ac5ce15cc41d943f
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-10-03 05:24:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ max_length = 2000
 # Initialize an empty string
 random_string = ""
 
-# Generate random letters until the length reaches max_length
 while len(random_string) < max_length:
-    random_letter = random.choice(string.ascii_letters)
-    random_string += random_letter
+    random_number = hrng_library.get_random_number()
+    random_ascii_character = chr(random_number % 128)  # Ensure it's a valid ASCII character
+    random_string += random_ascii_character
 
 random_numbers = [random.randint(0, 255) for _ in range(1000)]
 random_nummmy = ''.join(map(chr, random_numbers))
