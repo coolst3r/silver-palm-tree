7dbc5d998be856df0c456ff3454632e7fdaa9701
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-10-03 05:20:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,5 +13,7 @@ while len(random_string) < max_length:
     random_letter = random.choice(string.ascii_letters)
     random_string += random_letter
 
+random_numbers = [random.randint(0, 255) for _ in range(1000)]
+random_nummmy = ''.join(map(chr, random_numbers))
 
-print(random_string)
+print(random_string+random_nummy)
