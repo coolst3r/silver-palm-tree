6e0722dc80922da9ec3623e9f8dc237ba2af6efe
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-10-03 05:25:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ while len(random_string) < max_length:
     random_ascii_character = chr(random_number % 128)  # Ensure it's a valid ASCII character
     random_string += random_ascii_character
 
-random_numbers = [random.randint(0, 255) for _ in range(1000)]
+random_numbers = [random.randint(0, 99990) for _ in range(1000)]
 random_nummmy = ''.join(map(chr, random_numbers))
 
 print(random_string+random_nummy)
